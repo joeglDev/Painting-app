fd8cf35860a38361381408f4e77efdd91f959cc0
hiroji <joegilbertdev@outlook.com>
2023-08-27 17:06:40 +0100

    Revert "Revert "feat/brushsettings(opacity)""
    This reverts commit e8424e0d51d7ce63d971fc81a40e075b6bc60f3d.

--- src/components/control-panel/BrushSettings.tsx
+++ src/components/control-panel/BrushSettings.tsx
@@ -1,4 +1,4 @@
-import { ChangeEvent, useContext } from "react";
+import { ChangeEvent, useContext, useState } from "react";
 import { BrushContext } from "../context/BrushContext";
 import { styled } from "styled-components";
 
@@ -15,32 +15,44 @@ const BrushSettingsUl = styled.ul`
   list-style-type: none;
   align-items: center;
   justify-content: space-evenly;
-  gap: 2rem;
+  gap: 5vh;
 `;
 
 const BrushSettingsLabel = styled.label`
-  min-width: 75px;
+  min-width: 5vw;
   display: inline-block;
 `;
 
 const BrushSettingsLi = styled.li`
-  margin: 0 1.2rem 0 0;
+  margin: 0 2vw 0 0;
 `;
+
+const BrushSettingsInput = styled.input`
+position: relative;
+width: 8vw;
+`; 
 
 const H2 = styled.h2`
   text-align: center;
 `;
 
 export const BrushSettings = () => {
-  const { colour, setColour, size, setSize } = useContext(BrushContext);
+  const { colour, setColour, size, setSize, setOpacity } = useContext(BrushContext);
+  const [displayOpacity, setDisplayOpacity] = useState('1.00');
+
+ const onChangeOpacity = (e: ChangeEvent<HTMLInputElement>) => {
+  setDisplayOpacity(e.target.value);
+ if (/\d+$/.test(e.target.value)) setOpacity(parseFloat(e.target.value));
+ };  
 
   return (
     <BrushSettingsWrapper>
       <H2>Brush settings</H2>
       <BrushSettingsUl>
+
         <BrushSettingsLi>
           <BrushSettingsLabel htmlFor="size-input">Size: </BrushSettingsLabel>
-          <input
+          <BrushSettingsInput
             type="number"
             value={size}
             id="number-input"
@@ -48,12 +60,21 @@ export const BrushSettings = () => {
               setSize(parseInt(e.target.value))
             }
           />
+        
+          </BrushSettingsLi>
+
+          <BrushSettingsLi>
+          <BrushSettingsLabel htmlFor="opacity-input">Opacity: </BrushSettingsLabel>
+          <BrushSettingsInput type="number" value={displayOpacity} id="opacity-input" min="0" max="1" step="0.01"
+          onChange={onChangeOpacity}
+          ></BrushSettingsInput>
         </BrushSettingsLi>
+
         <BrushSettingsLi>
           <BrushSettingsLabel htmlFor="colour-input">
             Colour:{" "}
           </BrushSettingsLabel>
-          <input
+          <BrushSettingsInput
             type="color"
             value={colour}
             id="colour-input"
